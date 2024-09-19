a9f02da13eceaa8e98cfb50da858206a4374b1b5
Stan <93628110+wka791225@users.noreply.github.com>
2024-09-19 16:46:43 +0800

    fix:CUD 製作

--- app/Http/Controllers/Frontend/ModelTestController.php
+++ app/Http/Controllers/Frontend/ModelTestController.php
@@ -12,63 +12,62 @@ use Illuminate\Database\Eloquent\Model;
 use ReflectionClass;
 use ReflectionMethod;
 use Illuminate\Database\Eloquent\Relations\Relation;
-use Illuminate\Support\Facades\Validator;
 
 class ModelTestController extends Controller
 {
     //
     public function index()
     {
-         // 获取所有模型及其字段信息
-         $modelsPath = app_path('Models');
-         $modelFiles = File::allFiles($modelsPath);
-         $modelsData = [];
- 
-         foreach ($modelFiles as $file) {
-             $modelName = pathinfo($file, PATHINFO_FILENAME);
-             $modelClass = "App\\Models\\$modelName";
- 
-             if (class_exists($modelClass) && is_subclass_of($modelClass, Model::class)) {
-                 $modelInstance = new $modelClass;
-                 $tableName = $modelInstance->getTable();
-                 $columns = Schema::getColumnListing($tableName);
- 
-                 $columnsData = [];
-                 foreach ($columns as $column) {
-                     //  不要顯示的欄位
-                     if (in_array($column, ['id', 'created_at', 'updated_at'])) {
-                         continue;
-                     }
-                     $columnType = Schema::getColumnType($tableName, $column);
-                     $columnDetails = DB::select("SHOW FULL COLUMNS FROM $tableName LIKE '$column'")[0];
- 
-                     $columnList = [
-                         'name' => $column,
-                         'type' => $columnType,
-                         'nullable' => $columnDetails->Null === 'YES',
-                         'comment' => $columnDetails->Comment,
-                         'required' => false,
-                         'requestName' => '',
-                     ];
-                     array_push($columnsData, $columnList);
-                 }
- 
-                 $relations = $this->getModelRelations($modelInstance);
-                 $array = [
-                     'modelName' => $modelName,
-                     'columns' => $columnsData,
-                     'relations' => $relations,
-                 ];
-                 array_push($modelsData, $array);
-             }
-         }
- 
-         $data = [
-             'model' => $modelsData,
-         ];
+        // 获取所有模型及其字段信息
+        $modelsPath = app_path('Models');
+        $modelFiles = File::allFiles($modelsPath);
+        $modelsData = [];
+
+        foreach ($modelFiles as $file) {
+            $modelName = pathinfo($file, PATHINFO_FILENAME);
+            $modelClass = "App\\Models\\$modelName";
+
+            if (class_exists($modelClass) && is_subclass_of($modelClass, Model::class)) {
+                $modelInstance = new $modelClass;
+                $tableName = $modelInstance->getTable();
+                $columns = Schema::getColumnListing($tableName);
+
+                $columnsData = [];
+                foreach ($columns as $column) {
+                    //  不要顯示的欄位
+                    if (in_array($column, ['id', 'created_at', 'updated_at'])) {
+                        continue;
+                    }
+                    $columnType = Schema::getColumnType($tableName, $column);
+                    $columnDetails = DB::select("SHOW FULL COLUMNS FROM $tableName LIKE '$column'")[0];
+
+                    $columnList = [
+                        'name' => $column,
+                        'type' => $columnType,
+                        'nullable' => $columnDetails->Null === 'YES',
+                        'comment' => $columnDetails->Comment,
+                        'required' => false,
+                        'requestName' => '',
+                    ];
+                    array_push($columnsData, $columnList);
+                }
+
+                $relations = $this->getModelRelations($modelInstance);
+                $array = [
+                    'modelName' => $modelName,
+                    'columns' => $columnsData,
+                    'relations' => $relations,
+                ];
+                array_push($modelsData, $array);
+            }
+        }
+
+        $data = [
+            'model' => $modelsData,
+        ];
         return Inertia::render('Frontend/Index', ['response' => rtFormat($data)]);
     }
-
+    // 取得模型關聯
     private function getModelRelations($modelInstance)
     {
         $relations = [];
@@ -93,52 +92,155 @@ class ModelTestController extends Controller
 
         return $relations;
     }
-    public function doController(Request $request) 
+    // 生成控制器
+    public function doController(Request $request)
     {
 
         // 建立controller
+
+        $pathTest = 'public function __construct(protected FilesService $filesService,)
+        {
+        }'."\n";
         // 寫 validate
-        
         $data = $request->all();
-        $validationRules = $this->generateValidationRules($data);
+        $validationString = $this->generateValidationString($data);
+        // 後臺資料列，後續處理
 
-        $validator = Validator::make($request->all(), $validationRules);
-        dd($validationRules );
-
+        // 新增
+        $createSql = $this->generateInsertSQL($data);
+        $create = 'public function store(Request $request){'. "\n" . $validationString . "\n" . $createSql."\n" ."}";
+        // 編輯 
+        $editSql = $this->generateEditSQL($data);
+        $edit = 'public function update(Request $request){'. "\n" . $validationString . "\n" . $editSql."\n" ."}";
+        // 刪除
+        $delete = $this->generateDeleteSQL($data);
+        // 合併所有字串，並製作成php檔案
+        $allText = $pathTest."\n" .$create."\n" .$edit."\n" .$delete;
+        
         
     }
     // 驗證字串
-    private function generateValidationRules($data)
+    private function generateValidationString($data)
     {
         $rules = [];
 
         foreach ($data as $table) {
             foreach ($table['columns'] as $column) {
-                $rule = [];
-
+                // 只有當 $column['required'] 為 true 時才寫入驗證規則
                 if ($column['required']) {
-                    $rule[] = 'required';
-                } else if (!$column['nullable']) {
-                    $rule[] = 'required';
-                }
+                    $rule = ['required'];
 
-                switch ($column['type']) {
-                    case 'string':
-                        $rule[] = 'string';
-                        break;
-                    case 'date':
-                        $rule[] = 'date';
-                        break;
-                    case 'text':
-                        $rule[] = 'string';
-                        break;
-                    // 根據需要添加更多類型
-                }
+                    switch ($column['type']) {
+                        case 'integer':
+                            $rule[] = 'integer';
+                            break;
+                        case 'string':
+                            $rule[] = 'string';
+                            break;
+                        case 'date':
+                            $rule[] = 'date';
+                            break;
+                        case 'text':
+                            $rule[] = 'string';
+                            break;
+                            // 根據需要添加更多類型
+                    }
 
-                $rules[$column['requestName']] = implode('|', $rule);
+                    // 將規則組合成一個字串
+                    $rules[$column['requestName']] = implode('|', $rule);
+                }
             }
         }
 
-        return $rules;
+        // 手動構建驗證規則字串
+        $validationString = "\$request->validate([\n";
+        foreach ($rules as $field => $rule) {
+            $validationString .= "  '$field' => '$rule',\n";
+        }
+        $validationString .= "]);";
+
+        return $validationString;
+    }
+    // 新增字串
+    private function generateInsertSQL($data)
+    {
+        $createStrings = [];
+
+        foreach ($data as $table) {
+            $fields = [];
+            $tableName = $table['tableName'];
+            $text = '';
+            foreach ($table['columns'] as $column) {
+                $fieldName = $column['name'];
+                $requestName = $column['requestName'];
+
+                if ($requestName === 'filePath') {
+                    $text = "\$file = \$this->filesService->fileUpload(\$request->$requestName, '$tableName');\$filePath = \$file->path;" . "\n";
+                    $value = "'\$filePath'";
+                } elseif ($column['type'] === 'date') {
+                    $value = "date('Y-m-d', strtotime(\$request->$requestName))";
+                } else {
+                    $value = "\$request->$requestName";
+                }
+                $fields[] = "'$fieldName' => $value" ;
+            }
+
+            $createString = $text . $table['tableName'] . "::create([" . implode(', '. "\n", $fields) . "]);";
+            $createStrings[] = $createString;
+        }
+
+        return implode("\n", $createStrings);
+    }
+    // 編輯字串
+    private function generateEditSQL($data)
+    {
+        $editSQLStrings = [];
+
+        $setClauses = [];
+        foreach ($data as $table) {
+            $tableId = '$'.strtolower($table['tableName']).'= '.$table['tableName'].'::find($request->id);';
+            $tableName = $table['tableName'];
+            $text = '';
+            foreach ($table['columns'] as $column) {
+                $fieldName = $column['name'];
+                $requestName = $column['requestName'];
+
+                if ($requestName === 'filePath') {
+                    $text = "\$file = \$this->filesService->fileUpload(\$request->$requestName, '$tableName');if(\$file){\$filePath = \$file->path;}else{\$filePath = \$request->$requestName}" . "\n";
+                    $value = "'\$filePath'";
+                } elseif ($column['type'] === 'date') {
+                    $value = "date('Y-m-d', strtotime(\$request->$requestName))";
+                } elseif($requestName === 'pathName') {
+                    $value = '$file->name';
+                } else {
+                    $value = "\$request->$requestName";
+                }
+                $setClauses[] = "'$fieldName' => $value" ;
+            }
+
+            $editSQL =$tableId. "\n".$text. '$'.strtolower($table['tableName']). "->update([" . implode(', '. "\n", $setClauses) . "]);";
+            $editSQLStrings[] = $editSQL;
+        }
+
+        return implode("\n", $editSQLStrings);
+
+    }
+    // 刪除字串
+    private function generateDeleteSQL($data)
+    {        $delete = 'public function destroy($id){' . "\n";
+        // 檢查是否有 filePath 字段
+        foreach ($data as $table) {
+            $delete .= '$' . strtolower($table['tableName']) . '= ' . $table['tableName'] . '::find($id);' . "\n";
+            foreach ($table['columns'] as $column) {
+                if ($column['requestName'] === 'filePath') { 
+                    $delete .= '$this->filesService->fileDelete($'.strtolower($table['tableName']).'->filePath);' . "\n";
+                }
+            }
+            $delete .= '$' . strtolower($table['tableName']) . '->delete();' . "\n";
+        }
+        
+        $delete .= '}';
+
+        return $delete;
     }
 }
